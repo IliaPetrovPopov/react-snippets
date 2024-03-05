592ed2abf0427e268be261e6bb38cf31a704ade7
Ilia Popov <ilia_popov02@abv.bg>
2024-03-05 08:59:49 +0200

    Add configuratio to tailwind

--- tailwind-practice/tailwind.config.js
+++ tailwind-practice/tailwind.config.js
@@ -2,19 +2,19 @@
 // eslint-disable-next-line
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  // theme: {
-  //   fontFamily: {
-  //     sans: 'Roboto Mono, monospace',
-  //   },
+  theme: {
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
 
-    // extend: {
-    //   fontSize: {
-    //     huge: ['80rem', { lineHeight: '1' }],
-    //   },
-    //   height: {
-    //     screen: '100dvh',
-    //   },
-    // },
-  // },
+    extend: {
+      fontSize: {
+        huge: ['80rem', { lineHeight: '1' }],
+      },
+      height: {
+        screen: '100dvh',
+      },
+    },
+  },
   plugins: [],
 };
