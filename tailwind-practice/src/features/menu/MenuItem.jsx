5ad350880b0d724e0dafdcbb2d402c832e737701
Ilia Popov <ilia_popov02@abv.bg>
2024-03-05 09:00:47 +0200

    Add styling to Menu & MenuItem

--- tailwind-practice/src/features/menu/MenuItem.jsx
+++ tailwind-practice/src/features/menu/MenuItem.jsx
@@ -5,22 +5,22 @@ function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li className="">
+    <li className="flex gap-4 py-2">
       <img
         src={imageUrl}
         alt={name}
-        // className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
       />
-      <div className="">
-        <p className="">{name}</p>
-        <p className="">
+      <div className="flex grow flex-col pt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
           {ingredients.join(', ')}
         </p>
-        <div className="">
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
-            <p className="">{formatCurrency(unitPrice)}</p>
+            <p className="text-sm ">{formatCurrency(unitPrice)}</p>
           ) : (
-            <p className="">
+            <p className="text-sm font-medium uppercase text-stone-500">
               Sold out
             </p>
           )}
