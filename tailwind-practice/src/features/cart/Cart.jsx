9f0bf026e36eaaf0f6efc42dd9b42b0932c9ee86
Ilia Popov <ilia_popov02@abv.bg>
2024-03-05 09:00:29 +0200

    Add styling to Card & CartItem

--- tailwind-practice/src/features/cart/Cart.jsx
+++ tailwind-practice/src/features/cart/Cart.jsx
@@ -31,18 +31,18 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div className="">
+    <div className="px-4">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <ul className="">
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
           <CartItem item={item} key={item.key} />
         ))}
       </ul>
 
-      <div className="">
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
--- tailwind-practice/src/features/cart/CartItem.jsx
+++ tailwind-practice/src/features/cart/CartItem.jsx
@@ -5,12 +5,12 @@ function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li className="">
-      <p className="">
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div className="">
-        <p className="">{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
         <Button type="small">Delete</Button>
       </div>
     </li>
